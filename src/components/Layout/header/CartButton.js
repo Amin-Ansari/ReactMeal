c804a2219271d311a7505c0bff9d13b2cf55e909
Amin Ansari <devansari@yahoo.com>
2023-04-20 11:46:11 +0330

    The bump animation is added

--- src/components/Layout/header/CartButton.js
+++ src/components/Layout/header/CartButton.js
@@ -2,12 +2,29 @@ import { FaShoppingCart } from "react-icons/fa";
 import MealContext from "../../store/meal-context";
 import React from "react";
 import "./CartButton.css";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const CartButton = (props) => {
   const mealsTotalAmount = React.useContext(MealContext);
+  const [animaitonState, updateAnimationState] = useState(false);
+  let animationClass = "";
+  useEffect(() => {
+    if (mealsTotalAmount.foodItems.length) {
+      updateAnimationState(true);
+    }
+    setTimeout(() => {
+      updateAnimationState(false);
+    }, 300);
+  }, [mealsTotalAmount.totalAmount]);
+
+  animationClass = `${animaitonState ? "bump" : ""}`;
 
   return (
-    <button className="button-style " onClick={props.onTogglingCart}>
+    <button
+      className={`button-style ${animationClass}`}
+      onClick={props.onTogglingCart}
+    >
       <FaShoppingCart />
       <span className="hide-on-mobile">Cart</span>
       <span className="stock-number">{mealsTotalAmount.totalAmount}</span>
