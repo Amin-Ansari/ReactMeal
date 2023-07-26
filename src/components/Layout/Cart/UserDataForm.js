5fc274621460e136342aa2245c6b534a8715fc0f
Amin Ansari <devansari@yahoo.com>
2023-07-26 13:31:11 +0330

    The input validation of the form is done

--- src/components/Layout/Cart/UserDataForm.js
+++ src/components/Layout/Cart/UserDataForm.js
@@ -1,26 +1,15 @@
 import React, { useState, useReducer } from "react";
+import FormInput from "./FormInput";
 import "./TotalPrice.css";
 
 const UserDataForm = (props) => {
   return (
     <div className="form-container">
       <form className="user-data-form">
-        <label>
-          Name:
-          <input></input>
-        </label>
-        <label>
-          Full address:
-          <input></input>
-        </label>
-        <lable>
-          Postal code:
-          <input></input>
-        </lable>
-        <label>
-          City:
-          <input></input>
-        </label>
+        <FormInput label="Name: " />
+        <FormInput label="Full Addres: " />
+        <FormInput label="Postal Code: " />
+        <FormInput label="City: " />
       </form>
     </div>
   );
